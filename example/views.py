611052a7b283b9fccef61354d486ca16a462545c
aqwertyuiop48 <aqwertyuiop48@outlook.com>
2026-03-14 17:41:46 +0530

    Update internal URLs

--- example/views.py
+++ example/views.py
@@ -2226,7 +2226,7 @@ def maze(request):
       };
       sprite = new Image();
       sprite.src =
-        "https://image.ibb.co/dr1HZy/Pf_RWr3_X_Imgur.png" +
+        "https://i.imgur.com/PfRWr3X.png" +
         "?" +
         new Date().getTime();
       sprite.setAttribute("crossOrigin", " ");
@@ -2238,7 +2238,7 @@ def maze(request):
       };
     
       finishSprite = new Image();
-      finishSprite.src = "https://image.ibb.co/b9wqnJ/i_Q7m_U25_Imgur.png"+
+      finishSprite.src = "https://i.imgur.com/iQ7mU25.png"+
       "?" +
       new Date().getTime();
       finishSprite.setAttribute("crossOrigin", " ");
